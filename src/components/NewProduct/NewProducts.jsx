0cc48f417dadfe00b20632d766b120cc817b2a08
Handa <anandamuhammadmtq@gmail.com>
2021-02-11 12:52:37 +0800

    Change port endpoint

--- src/components/NewProduct/NewProducts.jsx
+++ src/components/NewProduct/NewProducts.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import CollectionItem from "../CollectionItem/CollectionItem";
 
 import css from "./NewProducts.module.css";
-const url = "http://34.234.195.75:3000";
+const url = "http://34.234.195.75:8000";
 
 class NewProducts extends React.Component {
   constructor() {
